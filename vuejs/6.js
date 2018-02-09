88b6c7fe3b081a5e9074bff2a06af21461e60bb8
Salamon Andor <salamon.andor@gmail.com>
2018-02-09 14:41:47 +0100

    Add new files Signed-off-by: Salamon Andor <salamon.andor@gmail.com>

---
+++ vuejs/6.js
@@ -0,0 +1,9 @@
+var vm = new Vue ({
+   el: '#vue_det',
+   data: {
+       firstname: 'Ria',
+       lastname: 'Singh',
+       htmlcontent: '<div><h1>Vue JS Template</h1></div>',
+       imgsrc: 'img.jpg'
+   }
+});
